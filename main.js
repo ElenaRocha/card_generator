7372102e8a0d53339095619c55f184c031782cc4
Elena Rocha <ele_r.v@hotmail.com>
2023-09-25 22:07:53 +0200

    capturando información de formulario

---
+++ main.js
@@ -0,0 +1,17 @@
+function generateCard() {
+    const userNameInput = document.querySelector("#name");
+    const userSurname1Input = document.querySelector("#surname1");
+    const userSurname2Input = document.querySelector("#surname2");
+    const userOcupationInput = document.querySelector("#ocupation");
+    const userRegistrationInput = document.querySelector("#registration_date");
+    const userExpirationInput = document.querySelector("#expiratoin_date");
+    //falta capturar la información sobre membresía VIP, que es con radio buttons
+
+    const userName = userNameInput.value;
+    const userSurname1 = userSurname1Input.value;
+    const userSurname2 = userSurname2Input.value;
+    const userOcupation = userOcupationInput.value;
+    const userRegistration = userRegistrationInput.value;
+    const userExpiration = userExpirationInput.value;
+}
+
